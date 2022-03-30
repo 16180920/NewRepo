18679c5ddeca9940010bd24d7327915a415749e8
mike <hmgs201297@gmail.com>
2022-03-29 18:50:14 -0600

    add pasw DB

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php 
  	//$conexion = mysqli_connect("localhost","root", "","proyecto");
-	$conexion = new mysqli("localhost","root","","tiendadeportiva");
+	$conexion = new mysqli("localhost","root","12345678","tiendadeportiva");
  	if ($conexion -> connect_error) {
  		echo "no hay conexion";
  	}
